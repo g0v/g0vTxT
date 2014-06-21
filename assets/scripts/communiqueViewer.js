1cf9f6fa677a699b6e5a9f7ca1078ad2046c6a36
Lee <jessy1092@gmail.com>
2014-06-21 18:05:37 +0800

    Add CommuniqueViewer.css and improve communique view.

--- assets/scripts/communiqueViewer.js
+++ assets/scripts/communiqueViewer.js
@@ -3,17 +3,22 @@ function CommuniqueCtrl($scope, $http, $element) {
     headers: {'Content-type': 'application/json'}})
   .success(function (data) {
     $scope.tagList = data;
+  });
+
+  $http.get('http://g0v-communique-api.herokuapp.com/api/1.0/entry/all?limit=50', {
+    headers: {'Content-type': 'application/json'}})
+  .success(function (data) {
+    $scope.communiqueTitle = 'All';
+    $scope.communiqueList = data;
   })
 
   $scope.showCommunique = function (tagName) {
     $http.get('http://g0v-communique-api.herokuapp.com/api/1.0/entry/' + tagName, {
       headers: {'Content-type': 'application/json'}})
     .success(function (data) {
-      // $element.find('#showCommunique').html = data.content;
       $scope.communiqueTitle = tagName;
       $scope.communiqueList = data;
-      // console.log(data);
-    })
+    });
   }
-  console.log("test");
+  console.log("communique test");
 }
